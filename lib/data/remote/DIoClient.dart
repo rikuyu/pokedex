b84fbd7f8d3a5ab85a027b0b24009c373b4bcfd3
rikuyu <rikuyu5884@gmail.com>
2023-07-23 18:54:17 +0900

    create dio client

---
+++ lib/data/remote/DIoClient.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final dioProvider = Provider((_) => DioClient().dio);
+
+class DioClient {
+  final Dio _dio = Dio();
+
+  DioClient() {
+    _dio.interceptors.add(LogInterceptor(responseBody: true));
+  }
+
+  Dio get dio => _dio;
+}
